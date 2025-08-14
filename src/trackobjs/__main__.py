0f26330ae30fd85f826b6ef45f46bd8611659430
Runqiu Bao <runqiu.bao@mujin.co.jp>
2025-08-14 18:22:41 +0900

    add logger from baodebug

--- src/trackobjs/__main__.py
+++ src/trackobjs/__main__.py
@@ -1,11 +1,16 @@
 import argparse
 import trackobjs
+import baodebug
+import os.path
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', type=str, required=True, help='path to the input images/, timestamps.txt, detections/.')
     args = parser.parse_args()
+
+    baodebug.debugutils.ConfigureRootLogger("info")  # config logger format
+    baodebug.debugutils.SetDebugPath(os.path.join(args.datadir, "baodebug/"))  # create debug folder here and set to os.environ["DEBUG_PATH"]
 
     trackobjs.track_objects(args.datadir)
 
